660f22c9db74560ae6c149793c82f66ecf70c768
kimheejoo <h32j00@gmail.com>
2020-12-31 03:44:26 +0900

    how to use dict, hash

---
+++ 완주하지못한선수.py
@@ -0,0 +1,45 @@
+# def solution(participant, completion):
+#     _par = [i for i in participant]
+#     _com = [i for i in completion]
+#     result = []
+#     for value in _par:
+#         if value not in _com:
+#             result.append(value)
+#         else:
+#             _com.pop(_com.index(value))
+#     return ''.join(result)
+
+# def solution(participant, completion):
+#     _par = {x:participant.count(x) for x in participant}
+#     _com = {x:completion.count(x) for x in completion}
+#     for key in _com.keys():
+#         _par[key]-=_com[key]
+#     for key in _par.keys():
+#         if _par[key]!=0: 
+#             result = key
+#             break
+#     return result
+
+# import collections
+# def solution(participant, completion):
+#     _par = collections.Counter(participant)
+#     _com = collections.Counter(completion)
+#     for key in _par.keys():
+#         if key in _com:
+#             _par[key]-=_com[key]
+#         if _par[key]!=0: 
+#             result = key
+#             break
+#     return result
+
+def solution(participant, completion):
+    temp = 0
+    dic = {}
+    for p in participant:
+        dic[hash(p)] = p
+        temp += hash(p)
+    for c in completion:
+        temp -= hash(c)
+    return dic[temp]
+
+print(solution(["leo","kiki","eden"],["leo","kiki"]))
