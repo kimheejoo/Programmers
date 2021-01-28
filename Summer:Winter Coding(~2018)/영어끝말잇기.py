1746d424c9954443f7ca0ea08d10a911acac50dd
kimheejoo <h32j00@gmail.com>
2021-01-28 15:05:13 +0900

    .

---
+++ Summer:Winter Coding(~2018)/영어끝말잇기.py
@@ -0,0 +1,12 @@
+def solution(n, words):
+    for i in range(1, len(words)):
+        if words[i] in words[:i] or words[i-1][-1] != words[i][0]:
+            th, who = divmod(i,n)
+            return [who+1, th+1]
+    else:
+        return [0,0]
+
+
+# print(solution(3, ['tank', 'kick', 'know', 'wheel', 'land', 'dream', 'mother', 'robot', 'tank']))
+print(solution(5,['hello', 'observe', 'effect', 'take', 'either', 'recognize', 'encourage', 'ensure', 'establish', 'hang', 'gather', 
+'refer', 'reference', 'estimate', 'executive']))
