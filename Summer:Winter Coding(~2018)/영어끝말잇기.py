f09356cd0a6ee868bb26b8629bf75a36b7ad45ba
kimheejoo <h32j00@gmail.com>
2021-01-28 15:21:24 +0900

    .

--- Summer:Winter Coding(~2018)/영어끝말잇기.py
+++ Summer:Winter Coding(~2018)/영어끝말잇기.py
@@ -6,7 +6,6 @@ def solution(n, words):
     else:
         return [0,0]
 
-
 # print(solution(3, ['tank', 'kick', 'know', 'wheel', 'land', 'dream', 'mother', 'robot', 'tank']))
 print(solution(5,['hello', 'observe', 'effect', 'take', 'either', 'recognize', 'encourage', 'ensure', 'establish', 'hang', 'gather', 
 'refer', 'reference', 'estimate', 'executive']))
